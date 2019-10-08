a2300f59494ca039001867e2023f586e636cec48
Andrii Baran <andriy.baran.v@gmail.com>
2019-10-08 12:24:05 +0300

    Added nested validation and improved specs

--- lib/steel_wheel/params.rb
+++ lib/steel_wheel/params.rb
@@ -8,18 +8,33 @@ module SteelWheel
       'SteelWheel::Params'
     end
 
-    %w(Integer Decimal Float Bool String Array Date DateTime Time).each do |type|
+    %w(Integer Decimal Float Bool String Array Date DateTime Time Struct).each do |type|
       self.singleton_class.define_method(type.downcase) { SteelWheel::Types.const_get(type) }
     end
 
-    def validate_array(attr_name)
-      public_send(attr_name).each.with_index do |element, i|
-        if element.invalid?
-          element.errors.each do |error_key,error_message|
-            errors.add(:"#{attr_name}[#{i}]_#{error_key}", error_message)
+    def validate_nested
+      run_nested_validations = proc do |attr_name, value, array_index, error_key_prefix|
+        case value
+        when Array
+          value.each.with_index do |element, i|
+            run_nested_validations[attr_name,element,i,error_key_prefix]
           end
+        when self.class.struct
+          if value.invalid?
+            error_key_components = [error_key_prefix, attr_name, array_index]
+            attr_error_key_prefix = error_key_components.compact.join('/')
+            value.errors.each do |error_key,error_message|
+              errors.add("#{attr_error_key_prefix}/#{error_key}", error_message)
+            end
+          end
+          value.attributes.each do |nested_attr_name, nested_value|
+            run_nested_validations[nested_attr_name,nested_value,nil,attr_error_key_prefix]
+          end
+        else
+          # NOOP
         end
       end
+      attributes.each(&run_nested_validations)
     end
   end
 end
--- spec/steel_wheel/params_spec.rb
+++ spec/steel_wheel/params_spec.rb
@@ -4,6 +4,31 @@ RSpec.describe SteelWheel::Params do
       Class.new(SteelWheel::Params) do
         attribute :id, integer
         attribute :quantity, integer.default(1)
+        attribute :sections, array.of(struct) do
+          attribute :id, integer
+          attribute :content, string.default('')
+          attribute :updated_at, date
+          attribute :post, struct do
+            attribute :id, integer
+            attribute :author, string.default('')
+            validates :id, presence: { message: "can't be blank" }
+          end
+          validates :id, presence: { message: "can't be blank" }
+        end
+        attribute :post, struct do
+          attribute :id, integer
+          attribute :author, string.default('')
+          validates :id, presence: { message: "can't be blank" }
+          attribute :sections, array.of(struct) do
+            attribute :id, integer
+            attribute :content, string.default('')
+            attribute :updated_at, date
+            attribute :meta, struct do
+              attribute :copies, array.of(string)
+            end
+            validates :id, presence: { message: "can't be blank" }
+          end
+        end
 
         validates :id, :quantity, presence: { message: "can't be blank" }
         validates :quantity, numericality: { only_integer: true, greater_than: 0 }
@@ -38,13 +63,90 @@ RSpec.describe SteelWheel::Params do
       end
     end
 
-    describe '.attributes' do
+    describe '.to_hash' do
       vars do
-        attributes { { id: 2, quantity: 5 } }
+        attributes do
+          {
+            id: 2,
+            quantity: 5,
+            sections: [
+              {
+                updated_at: '2018-07-13',
+                post: { author: 'Bob'}
+              }
+            ],
+            post: {
+              author: 'Bob',
+              sections: [
+                {
+                  updated_at: '2019-07-13',
+                  meta: {copies: []}
+                },
+                {
+                  updated_at: '2019-08-13',
+                  meta: {copies: []}
+                }
+              ]
+            }
+          }
+        end
+        attributes_with_defaults do
+          {
+            id: 2,
+            quantity: 5,
+            sections: [
+              {
+                content: '',
+                id: nil,
+                updated_at: Date.parse('2018-07-13'),
+                post: { id: nil, author: 'Bob'}
+              }
+            ],
+            post: {
+              author: 'Bob',
+              id: nil,
+              sections: [
+                {
+                  content: '',
+                  id: nil,
+                  updated_at: Date.parse('2019-07-13'),
+                  meta: {copies: []}
+                },
+                {
+                  content: '',
+                  id: nil,
+                  updated_at: Date.parse('2019-08-13'),
+                  meta: {copies: []}
+                }
+              ]
+            }
+          }
+        end
       end
 
       it 'returns hash with correct values' do
-        expect(params_obj.attributes).to eq attributes
+        expect(params_obj.to_hash).to eq attributes_with_defaults
+      end
+    end
+
+    describe '.validate_nested' do
+      vars do
+        attributes do
+          { id: 2, quantity: 5,
+            sections: [{updated_at: '2018-07-13', post: { author: 'Bob'}}],
+            post: { author: 'Bob', sections: [{updated_at: '2019-07-13'}] }
+          }
+        end
+      end
+
+      it 'returns hash with correct values' do
+        params_obj.validate_nested
+        expect(OpenStruct.new(params_obj.errors.messages)).to have_attributes(
+          :"sections/0/id"=>an_instance_of(Array),
+          :"sections/0/post/id"=>an_instance_of(Array),
+          :"post/id"=>an_instance_of(Array),
+          :"post/sections/0/id"=>an_instance_of(Array)
+        )
       end
     end
 
