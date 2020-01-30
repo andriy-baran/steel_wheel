bd48f7bb7463d8c9f5879f8292870a9ea691fc6e
Andrii Baran <andriy.baran.v@gmail.com>
2020-01-30 22:16:17 +0200

    Refactored composite module

--- lib/steel_wheel/composite.rb
+++ lib/steel_wheel/composite.rb
@@ -1,22 +1,5 @@
 module SteelWheel
   module Composite
-    module InvariableMethods
-      def __sw_subclass_error__(base_class)
-        -> { raise(ArgumentError, "must be a subclass of #{base_class.name}") }
-      end
-
-      def __sw_missing_body_error__
-        -> { raise(ArgumentError, 'please provide a block or class') }
-      end
-
-      def __sw_add_component_class_accessors__(method_name, component_name)
-        singleton_class.class_eval { attr_accessor :"#{method_name}_#{component_name}_class" }
-      end
-
-      def __sw_default_init__
-        ->(klass, *attrs) { klass.new }
-      end
-    end
     def self.[](component_name)
       mod = Module.new do
         class << self
@@ -27,9 +10,6 @@ module SteelWheel
           end
 
           def extended(receiver)
-            unless receiver.included_modules.include?(SteelWheel::Composite::InvariableMethods)
-              receiver.extend SteelWheel::Composite::InvariableMethods
-            end
             receiver.__sw_composites__[components_name] = component_name
           end
 
@@ -37,9 +17,65 @@ module SteelWheel
             :"__sw_store_#{title}_class__"
           end
 
-          def define_init(klass, init)
-            return klass if init.nil?
+          def activation_method(title = component_name)
+            :"__sw_activate_#{title}_component__"
+          end
+
+          def define_init(klass, &init)
+            return klass unless block_given?
             klass.singleton_class.send(:define_method, :__sw_init__, &init)
+          end
+
+          def patch_class(base_class, &block)
+            return base_class unless block_given?
+            Class.new(base_class, &block)
+          end
+
+          def check_inheritance!(base_class, component_class)
+            return if component_class.nil?
+            unless component_class <= base_class
+              raise(ArgumentError, "must be a subclass of #{base_class.name}")
+            end
+          end
+
+          def define_component_store_method
+            mod = self
+            define_method(:"#{mod.store_method}") do |method_name, klass|
+              public_send(:"#{method_name}_#{mod.component_name}_class=", klass)
+              public_send(:"#{mod.components_name}")[method_name] = klass
+            end
+          end
+
+          def define_component_activation_method
+            mod = self
+            define_method(:"__sw_activate_#{mod.component_name}_component__") do |method_name, base_class, klass, init = nil, &block|
+              component_class = public_send(:"#{method_name}_#{mod.component_name}_class") # inherited
+              raise(ArgumentError, 'please provide a block or class') if component_class.nil? && klass.nil? && block.nil?
+
+              target_class = component_class || klass || base_class
+
+              mod.check_inheritance!(base_class, target_class)
+
+              patched_class = mod.patch_class(target_class, &block)
+              mod.define_init(patched_class, &init)
+              public_send(mod.store_method, method_name, patched_class)
+            end
+          end
+
+          def define_component_adding_method
+            mod = self
+            default_init = ->(klass, *attrs) { klass.new }
+            define_method(component_name.to_sym) do |method_name, base_class: Class.new, init: default_init|
+              mod.define_init(base_class, &init)
+
+              singleton_class.class_eval do
+                attr_accessor :"#{method_name}_#{mod.component_name}_class"
+
+                define_method method_name do |klass = nil, init: nil, &block|
+                  public_send(mod.activation_method, method_name, base_class, klass, init, &block)
+                end
+              end
+            end
           end
         end
       end
@@ -67,42 +103,10 @@ module SteelWheel
             end
           end
         end
-
-        define_method(:"#{mod.store_method}") do |method_name, klass|
-          public_send(:"#{method_name}_#{mod.component_name}_class=", klass)
-          public_send(:"#{mod.components_name}")[method_name] = klass
-        end
-
-        define_method(:"__sw_activate_#{mod.component_name}_component__") do |method_name, base_class, klass, init = nil, &block|
-          component_class = public_send(:"#{method_name}_#{mod.component_name}_class")
-          if component_class.present? # inherited
-            __sw_subclass_error__(base_class).call unless component_class <= base_class
-
-            component_class = Class.new(component_class, &block) if !block.nil?
-            mod.define_init(component_class, init)
-            public_send(mod.store_method, method_name, component_class)
-          elsif klass.present? && block.nil?
-            __sw_subclass_error__(base_class).call unless klass <= base_class
-
-            mod.define_init(klass, init)
-            public_send(mod.store_method, method_name, klass)
-          elsif klass.nil? && !block.nil?
-            component_class = Class.new(base_class, &block)
-            mod.define_init(component_class, init)
-            public_send(mod.store_method, method_name, component_class)
-          else
-            __sw_missing_body_error__.call
-          end
-        end
-
-        define_method(mod.component_name.to_sym) do |method_name, base_class: Class.new, init: __sw_default_init__|
-          __sw_add_component_class_accessors__(method_name, mod.component_name)
-          mod.define_init(base_class, init)
-          singleton_class.send(:define_method, method_name) do |klass = nil, init: nil, &block|
-            public_send(:"__sw_activate_#{mod.component_name}_component__", method_name, base_class, klass, init, &block)
-          end
-        end
       end
+      mod.define_component_store_method
+      mod.define_component_activation_method
+      mod.define_component_adding_method
       mod
     end
   end
