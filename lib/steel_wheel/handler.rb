d4cf4c40b74d2176edeb8b4817b1412702f35e2b
Andrii Baran <andriy.baran.v@gmail.com>
2023-06-17 18:34:12 +0300

    Added success callbacks for individual steps

--- lib/steel_wheel/handler.rb
+++ lib/steel_wheel/handler.rb
@@ -19,14 +19,26 @@ module SteelWheel
       builders[flow].subclass(&block)
     end
 
+    def on_params_success(params)
+      # NOOP
+    end
+
     def on_params_failure(params)
       self.http_status = params.http_status
       errors.merge!(params.errors)
     end
 
+    def on_query_success(query)
+      # NOOP
+    end
+
     def on_query_failure(query)
       self.http_status = query.http_status
       errors.merge!(query.errors)
+    end
+
+    def on_command_success(command)
+      # NOOP
     end
 
     def on_command_failure(command)
@@ -42,12 +54,28 @@ module SteelWheel
     def self.handle(input:, flow: :main, &block)
       handler = new
       builder = builders[flow].with_callbacks do |c|
-        c.params { |o| handler.on_params_failure(o) if o.invalid? }
+        c.params do |o|
+          if o.invalid?
+            handler.on_params_failure(o)
+          else
+            handler.on_params_success(o)
+          end
+        end
         c.query do |o|
           block&.call(o)
-          handler.on_query_failure(o) if o.invalid?
+          if o.invalid?
+            handler.on_query_failure(o)
+          else
+            handler.on_query_success(o)
+          end
         end
-        c.command { |o| handler.on_command_failure(o) if o.invalid? }
+        c.command do |o|
+          if o.invalid?
+            handler.on_command_failure(o)
+          else
+            handler.on_command_success(o)
+          end
+        end
       end
       flow = builder.wrap(delegate: true) do |i|
         i.params(input)
